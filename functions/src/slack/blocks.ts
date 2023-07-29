c53fef589aeabd4e676423da662b604600d7da7e
Alex Meyer <acomeyer@gmail.com>
2023-07-29 17:17:43 -0500

    remove workflows

--- functions/src/slack/blocks.ts
+++ functions/src/slack/blocks.ts
@@ -3,11 +3,11 @@ import { HomeTab, Section, Button, Header } from 'slack-block-builder';
 
 export const appHomeView = (type: 'initial' | 'updated'): HomeView => {
   if (type === 'initial') {
-    return HomeTab().blocks(Header().text('Welcome to Team Digester!')).buildToObject();
+    return HomeTab().blocks(Header().text('Welcome to Digester!')).buildToObject();
   } else {
     return HomeTab()
       .blocks(
-        Section().text("Here's your dashboard"),
+        Header().text('Your dashboard'),
         Section()
           .text('You can add more blocks to your dashboard')
           .accessory(Button().text('Add a block').actionId('add_block'))
