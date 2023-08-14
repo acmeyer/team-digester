d90670c5ef6b6a64efaa0f502772c23c93dda590
Alex Meyer <acomeyer@gmail.com>
2023-08-13 22:09:16 -0500

    refactor webhooks into their own app

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import slackApp from './slack';
 import serverApp from './server';
+import webhooksApp from './webhooks';
 
 export const slack = slackApp;
 export const api = serverApp;
+export const webhooks = webhooksApp;
